4b35cbafa79d4d2998bd06383951a7f5ee5b0019
ninaglss15 <10526@holbertonstudents.com>
2025-03-26 22:46:50 +0100

    test printf

--- _printf.c
+++ _printf.c
@@ -28,7 +28,13 @@ int _printf(const char *format, ...)
 				count += print_percent(args);
 			else if (format[i] == 'd' || format[i] == 'i')
 				count += print_int(args);
-            		}
+			else
+        		{
+            			write(1, "%", 1);
+            			write(1, &format[i], 1);
+            			count += 2;
+        		}
+      		}
 		else
 		{
 			write(1, &format[i], 1);
