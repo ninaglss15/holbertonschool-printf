ce05d8bb132907f19c5283e8d1a596a9d8f6ab9d
omarrui <rouiguio919@gmail.com>
2025-03-30 21:47:20 +0200

    fixing func.c had an old code that wasnt removed

--- func.c
+++ func.c
@@ -1,50 +1,5 @@
 #include "main.h"
 /**
- * int_to_string - Convert an integer to a string
- * @num: The integer to convert
- * @buffer: A buffer to store the resulting string
- *
- * Return: A pointer to the buffer containing representation of the number
- */
-int int_to_string(int num, char *buffer)
-{
-	int i = 0, len = 0;
-	int j;
-	int negative_n = 0;
-
-	if (num == 0)
-	{
-	buffer[i++] = '0';
-	buffer[i] = '\0';
-	return (1);
-	}
-	else
-	{
-	if (num < 0)
-	{
-	negative_n = 1;
-	num = -num;
-	}
-	while (num > 0)
-	{
-	buffer[i++] = (num % 10) + '0';
-	num /= 10;
-	}
-	if (negative_n)
-	buffer[i++] = '-';
-	}
-	buffer[i] = '\0';
-	len = i;
-	for (j = 0; j < i / 2; j++)
-	{
-	char temp = buffer[j];
-
-	buffer[j] = buffer[i - j - 1];
-	buffer[i - j - 1] = temp;
-	}
-	return (len);
-}
-/**
  * my_strlen - Finds the length of a character string
  * @str: Pointer to the character string
  *
